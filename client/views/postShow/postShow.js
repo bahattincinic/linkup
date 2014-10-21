390aac4a282a4509a3371e9eefe2cd87d93198cb
Burak Alkan <burak.alkan@markafoni.com>
2014-10-21 14:16:52 +0300

    require post itself when insering message for it, cosmetic updates

--- client/views/postShow/postShow.js
+++ client/views/postShow/postShow.js
@@ -9,13 +9,18 @@ Template.postShow.events({
     var body = $.trim(form.find('textarea[name=body]').value);
 
     if (postId && body) {
-      Messages.insert({
-        postId: postId,
-        body: body
-      }, function(err, result) {
-        if (err)
-          throw err;
-      });
+      var post = Posts.findOne({_id: postId});
+      if (post) {
+        Messages.insert({
+          postId: postId,
+          body: body
+        }, function(err, result) {
+          if (err)
+            throw err;
+        });
+      } else {
+        throw new Meteor.Error('Invalid post id');
+      }
     }
   }
 });
