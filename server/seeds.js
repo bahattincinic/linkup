bd65a41bb278714a9215d077e96e0ff3fd1aac67
Burak Alkan <burak.alkan@markafoni.com>
2014-10-30 11:48:14 +0200

    fix seeds.js

--- server/seeds.js
+++ server/seeds.js
@@ -1,17 +1,18 @@
 Meteor.startup(function() {
-
-  if (Posts.find({}).count == 0) {
+  if (Posts.find().count() == 0) {
     _(10).times(function(n) {
       var post_id = Posts.insert({
         title: Fake.sentence(),
-        url: 'http://google.com'
+        url: 'http://google.com',
+        score: 0,
+        hot: 0
       });
 
       _(2).times(function(n) {
         Messages.insert({
           postId: post_id,
           body: Fake.paragraph()
-        })
+        });
       });
     })
   }
